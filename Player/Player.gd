ea36c9621bc084d9ef9ec67ec2c978e4f8ad5893
tyrelh <krankyjoe@gmail.com>
2021-01-30 09:25:44 -0800

    add accelleration and friction to player movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,7 +1,10 @@
 extends KinematicBody2D
 
+const MAX_PLAYER_MOVEMENT_SPEED = 100.0
+const ACCELERATION = 100
+const FRICTION = 100
+
 var velocity = Vector2.ZERO
-var PLAYER_MOVEMENT_SPEED = 2.0
 
 # Called when the node enters the scene tree for the first time.
 #func _ready():
@@ -11,24 +14,15 @@ var PLAYER_MOVEMENT_SPEED = 2.0
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 
-	input_vector.x = (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")) * PLAYER_MOVEMENT_SPEED
-	input_vector.y = (Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")) * PLAYER_MOVEMENT_SPEED
+	input_vector.x = (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"))
+	input_vector.y = (Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up"))
+	input_vector = input_vector.normalized()
 
 	if (input_vector != Vector2.ZERO):
-		print(input_vector)
-	velocity = input_vector if (input_vector != Vector2.ZERO) else Vector2.ZERO
-
-	# if Input.is_action_pressed("ui_right"):
-	# 	velocity.x = PLAYER_MOVEMENT_SPEED
-	# elif Input.is_action_pressed("ui_left"):
-	# 	velocity.x = -PLAYER_MOVEMENT_SPEED
-	# elif Input.is_action_pressed("ui_up"):
-	# 	velocity.y = -PLAYER_MOVEMENT_SPEED
-	# elif Input.is_action_pressed("ui_down"):
-	# 	velocity.y = PLAYER_MOVEMENT_SPEED
-	# else:
-	# 	velocity.x = 0
-	# 	velocity.y = 0
+		velocity += input_vector * ACCELERATION * delta
+		velocity = velocity.clamped(MAX_PLAYER_MOVEMENT_SPEED * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
 	move_and_collide(velocity)
 
