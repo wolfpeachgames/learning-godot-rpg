a587bcd41ce8b5e3f0ce343dd2544f3180fa3bae
tyrelh <krankyjoe@gmail.com>
2021-01-30 08:49:22 -0800

    basic player movement with arrow keys

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+var velocity = Vector2.ZERO
+var PLAYER_MOVEMENT_SPEED = 2.0
 
 # Called when the node enters the scene tree for the first time.
 #func _ready():
@@ -7,8 +9,28 @@ extends KinematicBody2D
 
 # Called every 'tick' that physics update. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_right"):
-		print("You pressed the right arrow key")
+	var input_vector = Vector2.ZERO
+
+	input_vector.x = (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")) * PLAYER_MOVEMENT_SPEED
+	input_vector.y = (Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")) * PLAYER_MOVEMENT_SPEED
+
+	if (input_vector != Vector2.ZERO):
+		print(input_vector)
+	velocity = input_vector if (input_vector != Vector2.ZERO) else Vector2.ZERO
+
+	# if Input.is_action_pressed("ui_right"):
+	# 	velocity.x = PLAYER_MOVEMENT_SPEED
+	# elif Input.is_action_pressed("ui_left"):
+	# 	velocity.x = -PLAYER_MOVEMENT_SPEED
+	# elif Input.is_action_pressed("ui_up"):
+	# 	velocity.y = -PLAYER_MOVEMENT_SPEED
+	# elif Input.is_action_pressed("ui_down"):
+	# 	velocity.y = PLAYER_MOVEMENT_SPEED
+	# else:
+	# 	velocity.x = 0
+	# 	velocity.y = 0
+
+	move_and_collide(velocity)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
