eeaf6910f35419a6170df75c3cfca2634e029ba7
tyrelh <krankyjoe@gmail.com>
2021-01-30 10:37:35 -0800

    simplify player movement code

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
 const MAX_PLAYER_MOVEMENT_SPEED = 100.0
-const ACCELERATION = 100
-const FRICTION = 100
+const ACCELERATION = 5000
+const FRICTION = 900
 
 var velocity = Vector2.ZERO
 
@@ -19,12 +19,11 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 
 	if (input_vector != Vector2.ZERO):
-		velocity += input_vector * ACCELERATION * delta
-		velocity = velocity.clamped(MAX_PLAYER_MOVEMENT_SPEED * delta)
+		velocity = velocity.move_toward(input_vector * MAX_PLAYER_MOVEMENT_SPEED, ACCELERATION * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
-	move_and_collide(velocity)
+	move_and_collide(velocity * delta)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
