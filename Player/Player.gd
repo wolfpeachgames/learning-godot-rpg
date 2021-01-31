5f4e64194beaab5b846f59fb4398da1d1ceb528c
tyrelh <krankyjoe@gmail.com>
2021-01-30 16:37:45 -0800

    basic player directional movement animations in code

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,9 +6,13 @@ const FRICTION = 900
 
 var velocity = Vector2.ZERO
 
+onready var animationPlayer = $AnimationPlayer
+
+
 # Called when the node enters the scene tree for the first time.
-#func _ready():
-#	print("Hello world")
+# func _ready():
+# 	pass
+
 
 # Called every 'tick' that physics update. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -19,13 +23,27 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 
 	if (input_vector != Vector2.ZERO):
+		animationPlayer.play(getMovementAnimationFromVector(input_vector))
 		velocity = velocity.move_toward(input_vector * MAX_PLAYER_MOVEMENT_SPEED, ACCELERATION * delta)
 	else:
+		animationPlayer.play("PlayerIdleDown")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
 	move_and_slide(velocity)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# func _process(delta):
+# 	pass
+
+func getMovementAnimationFromVector(vector):
+	var animation = "PlayerIdleDown"
+	if (vector.y < 0):
+		animation = "PlayerRunUp"
+	elif (vector.y > 0):
+		animation = "PlayerRunDown"
+	if (vector.x < 0):
+		animation = "PlayerRunLeft"
+	elif (vector.x > 0):
+		animation = "PlayerRunRight"
+	return animation
