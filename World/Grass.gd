9c4327d8ac54610263a9342d9d325d731c419fd6
tyrelh <krankyjoe@gmail.com>
2021-01-31 15:53:24 -0800

    add hurtbox to grass that destroys it

--- World/Grass.gd
+++ World/Grass.gd
@@ -4,11 +4,14 @@ extends Sprite
 const CONTROL_ATTACK = "attack"
 
 
-func _process(delta):
-	if (Input.is_action_just_pressed(CONTROL_ATTACK)):
-		var GrassEffect = load("res://Effects/GrassEffect.tscn")
-		var grassEffect = GrassEffect.instance()
-		var world = get_tree().current_scene
-		world.add_child(grassEffect)
-		grassEffect.global_position = global_position
-		queue_free()
+func create_grass_effect():
+	var GrassEffect = load("res://Effects/GrassEffect.tscn")
+	var grassEffect = GrassEffect.instance()
+	var world = get_tree().current_scene
+	world.add_child(grassEffect)
+	grassEffect.global_position = global_position
+
+
+func _on_Hurtbox_area_entered(area):
+	create_grass_effect()
+	queue_free()
