fda4a759c5c953c22314011e9f9b9cbf34eb6cb9
tyrelh <krankyjoe@gmail.com>
2021-02-01 08:26:06 -0800

    death effect for npcs

--- NPC/Bat.gd
+++ NPC/Bat.gd
@@ -4,19 +4,23 @@ const FRICTION = 200
 
 var knockback = Vector2.ZERO
 
+const NPCDeathEffect = preload("res://Effects/NPCDeathEffect.tscn")
+
 onready var stats = $Stats
 
-func _ready():
-	print(stats.max_health)
-
+# every physics tick
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
 	knockback = move_and_slide(knockback)
 
+# on hurtbox collision detection
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
 	knockback = area.knockback_vector * 100
 
-
+# on no health signal from stats
 func _on_Stats_no_health():
-	queue_free()
+	var npcDeathEffect = NPCDeathEffect.instance()
+	self.get_parent().add_child(npcDeathEffect)
+	npcDeathEffect.global_position = self.global_position
+	self.queue_free()
