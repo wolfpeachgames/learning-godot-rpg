ba3037c5b3ca84c293b2ac2e0b6a8828cc02baf7
tyrelh <krankyjoe@gmail.com>
2021-02-01 08:26:36 -0800

    add grass effect to parent node instead of world node

--- World/Grass.gd
+++ World/Grass.gd
@@ -4,10 +4,9 @@ const GrassEffect = preload("res://Effects/GrassEffect.tscn")
 
 func create_grass_effect():
 	var grassEffect = GrassEffect.instance()
-	var world = get_tree().current_scene
-	world.add_child(grassEffect)
+	get_parent().add_child(grassEffect)
 	grassEffect.global_position = global_position
 
 func _on_Hurtbox_area_entered(area):
 	create_grass_effect()
-	queue_free()
+	self.queue_free()
