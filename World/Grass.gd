c2beb73e1de593990517d7e4fc9130276370dab3
tyrelh <krankyjoe@gmail.com>
2021-02-01 08:19:33 -0800

    preload GrassEffect scene

--- World/Grass.gd
+++ World/Grass.gd
@@ -1,16 +1,12 @@
 extends Sprite
 
-
-const CONTROL_ATTACK = "attack"
-
+const GrassEffect = preload("res://Effects/GrassEffect.tscn")
 
 func create_grass_effect():
-	var GrassEffect = load("res://Effects/GrassEffect.tscn")
 	var grassEffect = GrassEffect.instance()
 	var world = get_tree().current_scene
 	world.add_child(grassEffect)
 	grassEffect.global_position = global_position
-
 
 func _on_Hurtbox_area_entered(area):
 	create_grass_effect()
