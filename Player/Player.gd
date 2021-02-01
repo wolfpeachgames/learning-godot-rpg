ac5a6ae8727062cca32c33ccf051ccdd47d80b6d
tyrelh <krankyjoe@gmail.com>
2021-01-31 18:02:01 -0800

    player roll animation and action

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,23 +1,27 @@
 extends KinematicBody2D
 
-const MAX_PLAYER_MOVEMENT_SPEED = 80
+const MAX_PLAYER_MOVEMENT_SPEED = 75
 const ACCELERATION = 5000
 const FRICTION = 900
+const PLAYER_ROLL_SPEED = 110
 
 const CONTROL_UP = "ui_up"
 const CONTROL_DOWN = "ui_down"
 const CONTROL_LEFT = "ui_left"
 const CONTROL_RIGHT = "ui_right"
 const CONTROL_ATTACK = "attack"
+const CONTROL_ROLL = "roll"
 
 const RUN_ANIMATION = "Run"
 const ATTACK_ANIMATION = "Attack"
 const IDLE_ANIMATION = "Idle"
+const ROLL_ANIMATION = "Roll"
 
 const BLEND_POSTITION_TEMPLATE_STRING = "parameters/%s/blend_position"
 const ATTACK_BLEND_POSTION = BLEND_POSTITION_TEMPLATE_STRING % ATTACK_ANIMATION
 const IDLE_BLEND_POSTION = BLEND_POSTITION_TEMPLATE_STRING % IDLE_ANIMATION
 const RUN_BLEND_POSTION = BLEND_POSTITION_TEMPLATE_STRING % RUN_ANIMATION
+const ROLL_BLEND_POSTION = BLEND_POSTITION_TEMPLATE_STRING % ROLL_ANIMATION
 
 enum {
 	MOVE_STATE,
@@ -27,6 +31,7 @@ enum {
 
 var state = MOVE_STATE
 var velocity = Vector2.ZERO
+var roll_vector = Vector2.DOWN
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
@@ -57,16 +62,20 @@ func move_state(delta):
 	input_vector = input_vector.normalized()
 
 	if (input_vector != Vector2.ZERO):
-		set_animation_blend_positions(input_vector)
-		animationState.travel(RUN_ANIMATION)
+		roll_vector = input_vector # save roll direction
+		set_animation_blend_positions(input_vector) # update direction for all animations
+		animationState.travel(RUN_ANIMATION) # initiate running animation
 		velocity = velocity.move_toward(input_vector * MAX_PLAYER_MOVEMENT_SPEED, ACCELERATION * delta)
 	else:
 		animationState.travel(IDLE_ANIMATION)
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
-	velocity = move_and_slide(velocity)
+	move()
 
-	if Input.is_action_just_pressed(CONTROL_ATTACK):
+	if (Input.is_action_just_pressed(CONTROL_ROLL)):
+		state = ROLL_STATE
+
+	if (Input.is_action_just_pressed(CONTROL_ATTACK)):
 		state = ATTACK_STATE
 
 
@@ -76,18 +85,28 @@ func attack_state(delta):
 
 
 func roll_state(delta):
-	pass
+	velocity = roll_vector * PLAYER_ROLL_SPEED
+	animationState.travel(ROLL_ANIMATION)
+	move()
 
 
 func set_animation_blend_positions(vector):
 	animationTree.set(IDLE_BLEND_POSTION, vector)
 	animationTree.set(RUN_BLEND_POSTION, vector)
 	animationTree.set(ATTACK_BLEND_POSTION, vector)
+	animationTree.set(ROLL_BLEND_POSTION, vector)
 
 
 func attack_animation_finished():
 	state = MOVE_STATE
 
+
+func roll_animation_finished():
+	state = MOVE_STATE
+
+
+func move():
+	velocity = move_and_slide(velocity)
 
 # Called every 'tick' that physics update. 'delta' is the elapsed time since the previous frame.
 # func _physics_process(delta):
