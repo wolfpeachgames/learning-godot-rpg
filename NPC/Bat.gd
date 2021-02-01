911a0fff7ec974523fdbf3c81aaea9d3a6a34722
tyrelh <krankyjoe@gmail.com>
2021-02-01 13:21:46 -0800

    bat state machine, bat fill follow player when near

--- NPC/Bat.gd
+++ NPC/Bat.gd
@@ -1,17 +1,47 @@
 extends KinematicBody2D
 
-const FRICTION = 200
+export var ACCELERATION = 250
+export var FRICTION = 100
+export var MAX_MOVEMENT_SPEED = 45
 
-var knockback = Vector2.ZERO
+enum { IDLE_STATE, WANDER_STATE, CHASE_STATE }
 
 const NPCDeathEffect = preload("res://Effects/NPCDeathEffect.tscn")
 
+var velocity = Vector2.ZERO
+var knockback = Vector2.ZERO
+var state = CHASE_STATE
+onready var sprite = $AnimatedSprite
 onready var stats = $Stats
+onready var playerDetectionZone = $PlayerDetectionZone
 
 # every physics tick
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
 	knockback = move_and_slide(knockback)
+
+	match state:
+		IDLE_STATE:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+			seek_player()
+		WANDER_STATE:
+			pass
+		CHASE_STATE:
+			var player  = playerDetectionZone.player
+			if (player != null):
+				var direction_vector = global_position.direction_to(player.global_position)
+				velocity = velocity.move_toward(direction_vector * MAX_MOVEMENT_SPEED, ACCELERATION * delta)
+			else:
+				state = IDLE_STATE
+			sprite.flip_h = velocity.x < 0
+	
+	
+	velocity = move_and_slide(velocity)
+
+
+func seek_player():
+	if (playerDetectionZone.can_see_player()):
+		state = CHASE_STATE
 
 # on hurtbox collision detection
 func _on_Hurtbox_area_entered(area):
