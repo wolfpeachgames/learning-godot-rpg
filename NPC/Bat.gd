c71aa5e690206719d84c3cc42fe1aebf11f3fbf6
tyrelh <krankyjoe@gmail.com>
2021-01-31 21:15:25 -0800

    stats manager for npcs, bats can die now

--- NPC/Bat.gd
+++ NPC/Bat.gd
@@ -4,10 +4,19 @@ const FRICTION = 200
 
 var knockback = Vector2.ZERO
 
+onready var stats = $Stats
+
+func _ready():
+	print(stats.max_health)
+
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
 	knockback = move_and_slide(knockback)
 
 func _on_Hurtbox_area_entered(area):
+	stats.health -= area.damage
 	knockback = area.knockback_vector * 100
-	# queue_free()
+
+
+func _on_Stats_no_health():
+	queue_free()
--- Player/SwordHitbox.gd
+++ Player/SwordHitbox.gd
@@ -1,3 +1,3 @@
-extends Area2D
+extends "res://Hitboxes and Hurtboxes/Hitbox.gd"
 
 var knockback_vector = Vector2.ZERO
