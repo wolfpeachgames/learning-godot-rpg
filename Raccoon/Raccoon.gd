6bf8ca70b6756637e977424c9ea861f4d578eb69
tyrelh <krankyjoe@gmail.com>
2021-02-14 10:38:09 -0800

    transition raccoon animation blends based on input

--- Raccoon/Raccoon.gd
+++ Raccoon/Raccoon.gd
@@ -10,9 +10,12 @@ const CONTROL_DOWN = "ui_down"
 const CONTROL_LEFT = "ui_left"
 const CONTROL_RIGHT = "ui_right"
 
-const WALK_ANIMATION = "PlayerWalkAnimation"
+const WALK_ANIMATION = "RaccoonWalk"
+const IDLE_ANIMATION = "RaccoonIdle"
+const IDLE_ANI_BLEND = "parameters/RaccoonIdle/blend_position"
+const WALK_ANI_BLEND = "parameters/RaccoonWalk/blend_position"
 
-export var MAX_PLAYER_MOVEMENT_SPEED = 75
+export var MAX_PLAYER_MOVEMENT_SPEED = 70
 const ACCELERATION = 5000
 const FRICTION = 900
 
@@ -26,36 +29,45 @@ onready var animationState = animationTree.get("parameters/playback")
 
 
 func _process(delta: float) -> void:
+	var input_vector = get_state_from_input()
 	match state:
 		MOVE_STATE:
-			do_move_state(delta)
+			do_move_state(input_vector, delta)
 		IDLE_STATE:
-			do_idle_state()
+			do_idle_state(delta)
+			
 
-
-func do_move_state(delta: float) -> void:
+func get_state_from_input() -> Vector2:
 	var input_vector = Vector2.ZERO
 	input_vector.x = (Input.get_action_strength(CONTROL_RIGHT) - Input.get_action_strength(CONTROL_LEFT))
 	input_vector.y = (Input.get_action_strength(CONTROL_DOWN) - Input.get_action_strength(CONTROL_UP))
 	input_vector = input_vector.normalized()
 	
 	if (input_vector != Vector2.ZERO):
-#		set_animation_blend_positions(input_vector) # update direction for all animations
-#		animationState.travel(WALK_ANIMATION) # initiate running animation
-		if (input_vector.x > 0):
-			animationPlayer.play("PlayerWalkRightAnimation")
-		elif (input_vector.x < 0):
-			animationPlayer.play("PlayerWalkLeftAnimation")
-		velocity = velocity.move_toward(input_vector * MAX_PLAYER_MOVEMENT_SPEED, ACCELERATION * delta)
+		state = MOVE_STATE
 	else:
-		animationPlayer.play("PlayerIdleLeftAnimation")
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		state = IDLE_STATE
+	return input_vector
+
+
+func do_move_state(input_vector: Vector2, delta: float) -> void:
+	set_animation_blend_positions(input_vector) # update direction for all animations
+	animationState.travel(WALK_ANIMATION) # initiate running animation
+	velocity = velocity.move_toward(input_vector * MAX_PLAYER_MOVEMENT_SPEED, ACCELERATION * delta)
 	move()
 
 
-func do_idle_state() -> void:
-	pass
+func do_idle_state(delta: float) -> void:
+	animationState.travel(IDLE_ANIMATION)
+	velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	move()
 	
 	
 func move() -> void:
 	velocity = move_and_slide(velocity)
+
+
+func set_animation_blend_positions(input_vector: Vector2) -> void:
+	animationTree.set(IDLE_ANI_BLEND, input_vector)
+	animationTree.set(WALK_ANI_BLEND, input_vector)
+	
